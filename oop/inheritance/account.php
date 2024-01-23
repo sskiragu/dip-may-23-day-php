cdfbb295575571a5400a33c929e5aafb32cd9334
sskiragu <skiragu@gmail.com>
2024-01-23 14:46:12 +0300

    Inheritance part 1

--- oop/inheritance/account.php
+++ oop/inheritance/account.php
@@ -3,13 +3,27 @@
 class Account{
 
     // properties
-    public $account_no = 123456789098;
-    public $account_name = "John Doe";
+    public $account_no;
+    public $account_name;
+
+    public function __construct($account_no, $account_name)
+    {
+        $this->account_no = $account_no;
+        $this->account_name = $account_name;
+    }
+
+    public function print_account_info(){
+        echo "{$this->account_no} {$this->account_name}";
+    }
 }
 
 class SavingAccount extends Account{
 
 }
 
-$saving_account = new SavingAccount();
-var_dump($saving_account);
+class FixedAccount extends Account{
+    public $interest = '1.5';
+}
+
+$fixed_account = new FixedAccount(23456789098765, "John Doe");
+$fixed_account->print_account_info();
